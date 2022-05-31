9a7d09630deef443a06525766d901a7c1a3319e3
firdavsDev <tarjimatv1@gmail.com>
2022-05-31 11:20:13 +0500

    uzgarish

--- api/v1/maps/serializers.py
+++ api/v1/maps/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 
 from .models import *
 
+class MapCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CategoryofMaps
+        fields = '__all__'
+
 class MapsSerializer(serializers.ModelSerializer):
+    category = MapCategorySerializer(read_only=True)
     class Meta:
         model = Maps
         fields = "__all__"
--- api/v1/services/admin.py
+++ api/v1/services/admin.py
@@ -9,7 +9,7 @@ from import_export.admin import ImportExportActionModelAdmin
 @admin.register(models.Service)
 class ServiceAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['id', 'name','service_type', 'created_at','is_active' ]
-    list_filter = ['is_active',]
+    list_filter = ['is_active','is_free']
     list_display_links = ('id', 'name', 'created_at')
     date_hierarchy = 'created_at'
     ordering = ['-created_at']
